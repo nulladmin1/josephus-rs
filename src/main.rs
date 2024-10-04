c997a372d0e1e5dbab4cfd3016515edb8a354513
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-04 19:01:35 -0400

    Completed Josephus

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::{io, usize};
 
 fn main() {
     let mut n = String::new();
@@ -7,17 +7,18 @@ fn main() {
     println!("Enter number of students: ");
     io::stdin().read_line(&mut n).expect("Failed to read line!");
     
-    let n: i8 = n.parse().expect("Not a number. ");
+    let n: usize = n.trim().parse().expect("Not a number. ");
 
     println!("Enter k'th student to skip: ");
     io::stdin().read_line(&mut k).expect("Failed to read line!");
 
-    let k: i8 = k.parse().expect("Not a number. ");
+    let k: usize = k.trim().parse().expect("Not a number. ");
 
     let mut s = 0;
-    let mut i = 1;
 
-    while (i <= n) {
-        s = 
+    for i in 1..=n {
+        s = (s + k) % i;
     }
+
+    println!("The winner is: {}", s+1);
 }
