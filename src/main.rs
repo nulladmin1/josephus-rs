fda44f5eb9c2eb0417cf5f265b97f16e654a6655
nulladmin1 <shrey.deogade@protonmail.com>
2024-10-04 07:15:24 -0400

    Added input for n

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut n = String::new();
+    let mut k = String::new();
+
+    println!("Enter number of students: ");
+    io::stdin().read_line(&mut n).expect("Failed to read line!");
 }
