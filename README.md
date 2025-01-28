ffa333901882bc3aa8db4339c355ef01af6c07b9
Shrey Deogade <shrey.deogade@protonmail.com>
2025-01-28 16:33:09 +0000

    Update project

---
+++ README.md
@@ -0,0 +1,16 @@
+# Josephus-rs
+An implementation of the [Josephus problem](https://en.wikipedia.org/wiki/Josephus_problem) in `Rust`
+
+## Run
+
+### Using `Nix`
+
+```sh
+nix run
+```
+
+### Using `Cargo`
+
+```sh
+cargo run
+```
--- flake.nix
+++ flake.nix
@@ -1,30 +1,45 @@
 {
-  description = "A very basic flake";
+  description = "josephus";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs, rust-overlay }: 
-  let
-    system = "x86_64-linux";
-    overlays = [(import rust-overlay)];
-    pkgs = import nixpkgs {
-      inherit overlays system;
-    };
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    pkgsFor = forEachSystem (system: import nixpkgs {inherit system;});
   in {
-    devShell = pkgs.mkShell {
-      CARGO_INSTALL_ROOT = "${toString ./.}/.cargo";
-      
-      buildInputs = with pkgs; [
-        cargo
-        rustc
-        rust-analyzer
-      ];
-    };
+    formatter = forEachSystem (system: pkgsFor.${system}.alejandra);
+
+    devShells = forEachSystem (system: {
+      default = pkgsFor.${system}.mkShell {
+        packages = with pkgsFor.${system}; [
+          rustc
+          cargo
+        ];
+      };
+    });
+
+    packages = forEachSystem (system: {
+      default = pkgsFor.${system}.rustPlatform.buildRustPackage {
+        pname = "josephus";
+        version = "0.1.0";
+        src = ./.;
+        cargoHash = "sha256-dJoe/rHDYT6yAMwKZAi4aMGgoAz5JGotke/QL5df/E4=";
+      };
+    });
+
+    apps = forEachSystem (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/josephus";
+      };
+    });
   };
 }
